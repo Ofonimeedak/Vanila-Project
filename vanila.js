3f512d83c13709f86f4eaa1d699952268a2f5136
Kofoworola Sogunle <ofonimeedak@gmail.com>
2025-03-08 19:10:15 +0000

    3rd review

--- vanila.js
+++ vanila.js
@@ -44,3 +44,22 @@ try {
 }
 }
 getProduct();
+
+// /To open and close the mobile navigation bar
+
+const myNavBar = document.getElementById("navBar");
+const openBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close");
+
+function openAndClose(){
+  if(myNavBar.style.display==="flex"){
+    myNavBar.style.display="none"
+  }
+
+  else{
+    myNavBar.style.display="flex"
+  }
+}
+
+openBtn.addEventListener('click', openAndClose);
+closeBtn.addEventListener('click',openAndClose);
